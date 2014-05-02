fb7f68256334a04f709a1d30bb0ae264b7b7dbe7
yearsgoing <yearsgoingbuy@gmail.com>
2014-05-02 23:22:02 +0300

    savo to sprdsh

--- learn-rails/app/controllers/contacts_controller.rb
+++ learn-rails/app/controllers/contacts_controller.rb
@@ -5,7 +5,7 @@ end
 def create
 @contact = Contact.new(secure_params)
 if @contact.valid?
-# TODO save data
+@contact.update_spreadsheet
 # TODO send message
 flash[:notice] = "Message sent from #{@contact.name}."
 redirect_to root_path
